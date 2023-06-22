ec798010584a715ed7ddee2156909fad8e249ed2
Luis Barreto <luis.barreto.marin@gmail.com>
2023-06-22 19:15:26 -0400

    Add: E2E for creating ride

--- UcabGo.App/ViewModel/GiveRide/DestinationsListViewModel.cs
+++ UcabGo.App/ViewModel/GiveRide/DestinationsListViewModel.cs
@@ -2,6 +2,8 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
 using UcabGo.App.Api.Services.Destinations;
+using UcabGo.App.Api.Services.Vehicles;
+using UcabGo.App.Models;
 using UcabGo.App.Services;
 using UcabGo.App.Views;
 using Location = UcabGo.App.Models.Location;
@@ -11,6 +13,7 @@ namespace UcabGo.App.ViewModel
     public partial class DestinationsListViewModel : ViewModelBase
     {
         readonly IDestinationsService destinationsService;
+        readonly IVehiclesApi vehiclesApi;
 
         [ObservableProperty]
         ObservableCollection<Location> destinations;
@@ -21,16 +24,35 @@ namespace UcabGo.App.ViewModel
         [ObservableProperty]
         string greeting;
 
+        [ObservableProperty]
+        bool isModalVisible;
 
-        public DestinationsListViewModel(ISettingsService settingsService, INavigationService navigation, IDestinationsService destinationsService) : base(settingsService, navigation)
+        [ObservableProperty]
+        ObservableCollection<Vehicle> vehicles;
+
+        [ObservableProperty]
+        Vehicle selectedVehicle;
+
+        [ObservableProperty]
+        int seatQuantity;
+
+        Location SelectedDestination { get; set; }
+
+        public DestinationsListViewModel(ISettingsService settingsService, INavigationService navigation, IDestinationsService destinationsService, IVehiclesApi vehiclesApi) : base(settingsService, navigation)
         {
-            destinations = new ObservableCollection<Location>();
             this.destinationsService = destinationsService;
+            this.vehiclesApi = vehiclesApi;
+
+            destinations = new();
+            vehicles = new();
         }
 
         public override async void OnAppearing()
         {
             Greeting = $"Hola, {settings.User.Name}.";
+            IsModalVisible = false;
+            SeatQuantity = 1;
+            
             await Refresh();
         }
 
@@ -40,13 +62,26 @@ namespace UcabGo.App.ViewModel
             Destinations.Clear();
             IsRefreshing = true;
 
-            var destinations = await destinationsService.GetDriverDestinations();
+            var taskDestinations = destinationsService.GetDriverDestinations();
+            var taskVehicles = vehiclesApi.GetVehicles();
+
+            await Task.WhenAll(taskDestinations, taskVehicles);
+
+            var destinations = await taskDestinations;
+
             if (destinations?.Message == "DESTINATIONS_FOUND")
             {
                 foreach (var destination in destinations.Data)
                 {
                     Destinations.Add(destination);
                 }
+            }
+
+            var vehicles = await taskVehicles;
+            if(vehicles?.Message == "VEHICLES_FOUND" && vehicles.Data.Any())
+            {
+                Vehicles = new(vehicles.Data);
+                SelectedVehicle = Vehicles[0];
             }
 
             IsRefreshing = false;
@@ -58,10 +93,38 @@ namespace UcabGo.App.ViewModel
             await navigation.NavigateToAsync<DestinationAddView>();
         }
         [RelayCommand]
-        async Task Start(Location destination)
+        void Configure(Location destination)
         {
-            //await navigation.NavigateToAsync<StartRideView>(destination);
+            IsModalVisible = true;
+            SelectedDestination = destination;
         }
+        [RelayCommand]
+        async Task Start()
+        {
+            //Display test message
+            await Application.Current.MainPage.DisplayAlert("Test", 
+                $"Ride {SelectedDestination.Alias} - {SeatQuantity} started", "Ok");
+        }
+        [RelayCommand]
+        void AddSeat()
+        {
+            SeatQuantity++;
+        }
+        [RelayCommand]
+        void RemoveSeat()
+        {
+            if (SeatQuantity > 1)
+            {
+                SeatQuantity--;
+            }
+        }
+
+        [RelayCommand]
+        async Task Cancel()
+        {
+            IsModalVisible = false;
+        }
+
         [RelayCommand]
         async Task Delete(Location destination)
         {
